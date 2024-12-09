db8b99f766093e00c1b92d4e7e3cf32e39f53523
sakibhasansufi <sakibhasansufi@gmail.com>
2024-12-10 00:03:45 +0600

    video is played in the background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
                         <div onClick={handleMiniVideoPlayer} className="origin-center scale-150 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                             <video
                                 ref={nextVideoRef}
-                                src={getVideoSrc(currentIndex + 1)}
+                                src={getVideoSrc(upcomingVideoIndex)}
                                 loop
                                 muted
                                 id="current-video"
@@ -41,6 +41,34 @@ const Hero = () => {
                             />
                         </div>
                     </div>
+                    <video
+                        ref={nextVideoRef}
+                        src={getVideoSrc(currentIndex)}
+                        loop
+                        muted
+                        id="next-video"
+                        className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+                        onLoadedData={handleVideoLoaded}
+                    />
+                    <video
+                        src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex + 1)}
+                        autoPlay
+                        loop
+                        muted
+                        className="absolute top-0 left-0 size-full object-cover object-center"
+                        onLoadedData={handleVideoLoaded}
+
+                    />
+                </div>
+                <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+                    G<b>a</b>ming
+                </h1>
+                <div className="absolute left-0 top-0 size-full">
+                    <div className="mt-24 px-5 sm:px-10">
+                        <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e</h1>
+                        <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the meta game Layer <br />Unleash the play economy</p>
+                    </div>
+
                 </div>
 
             </div>
