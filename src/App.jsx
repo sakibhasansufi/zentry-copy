72299002f5c9e00f806efe15519e42bf065bf1d2
sakibhasansufi <sakibhasansufi@gmail.com>
2024-12-11 20:48:10 +0600

    another video added with gsap animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500"/>
+      <About />
     </main>
   )
 }
---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,12 @@
+
+const AnimatedTitle = () => {
+    return (
+        <section>
+            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+                Disc<b>o</b>ver the world&apos;s <br /> l<b>a</b>rgest shared adventure
+            </div>
+        </section>
+    );
+};
+
+export default AnimatedTitle;
